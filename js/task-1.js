089c67378fc6a3053e6eb5fd66d72c9e75aa9f87
Lia <natalimail911@gmail.com>
2024-10-01 18:56:16 +0300

    task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,11 @@
 const list = document.querySelector("#categories");
-console.log(li.item); 
+console.log(`Number of categories: ${list.children.length}`); 
+
+const item = document.querySelectorAll(".item");
+item.forEach(element => {
+ 
+    console.log(`Category: ${element.firstElementChild.textContent}`);
+    console.log(`Elements: ${element.lastElementChild.children.length}`);
+    
+    
+})
